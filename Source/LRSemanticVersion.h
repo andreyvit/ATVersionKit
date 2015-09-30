2ed32880058488c105ad61b007d94691027c485c
Andrey Tarantsov <andrey@tarantsov.com>
2015-10-01 04:10:03 +0600

    🐞 Add missing nullability annotation

--- Source/LRSemanticVersion.h
+++ Source/LRSemanticVersion.h
@@ -5,7 +5,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface LRSemanticVersion : LRVersion
 
-- (id)initWithMajor:(NSInteger)major minor:(NSInteger)minor patch:(NSInteger)patch prereleaseComponents:(NSArray *)prereleaseComponents build:(NSString *)build error:(NSError *)error;
+- (id)initWithMajor:(NSInteger)major minor:(NSInteger)minor patch:(NSInteger)patch prereleaseComponents:(NSArray *)prereleaseComponents build:(NSString *)build error:(nullable NSError *)error;
 
 @property (nonatomic, readonly) NSInteger major;
 @property (nonatomic, readonly) NSInteger minor;
